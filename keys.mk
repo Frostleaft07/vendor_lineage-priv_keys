7cdc2c12e2ce680ca5474bc002b5331b21cfd134
Frostleaft07 <90104112+Frostleaft07@users.noreply.github.com>
2025-08-27 08:58:57 +0700

    update

--- keys.mk
+++ keys.mk
@@ -69,5 +69,5 @@ PRODUCT_CERTIFICATE_OVERRIDES += \
     ServiceWifiResources:com.android.wifi.resources.certificate.override \
     WifiDialog:com.android.wifi.dialog.certificate.override
 
-PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/lineage-priv/keys/testkey
+PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/extra/keys/releasekey
 PRODUCT_EXTRA_RECOVERY_KEYS :=
